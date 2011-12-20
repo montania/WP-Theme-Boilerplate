dee71225781d427cb7299961819e856f76bc3ffc
Rickard Andersson <rickard@montania.se>
2011-12-20 14:18:21 +0100

    Lagt till lite standardfunktioner, relative_sv och fixat wp-caption style

--- functions.php
+++ functions.php
@@ -1,25 +1,89 @@
 <?php 
+/**
+ * Main file for the WordPress Theme
+ * @author Rickard Andersson <rickard@montania.se>
+ * @copyright Montania System AB
+ * @version 1.0
+ */
 
-
+/**
+ * Main class for the WordPress Theme
+ * @author Rickard Andersson <rickard@montania.se>
+ */
 class Boilerplate {
 	
-	private $plugin_name = "boilerplate";
+	/**
+	 * Name of the directory where the theme files resides
+	 * @var string
+	 * @since 1.0
+	 */
+	private $theme_name = "boilerplate";
 	
+
+	/**
+	 * Will initialize the plugin function and add some hooks
+	 * @return void
+	 * @since 1.0
+	 */
 	function __construct() {
-		
+
+		add_action('init', array($this, 'init_menus'));
+		add_action('init', array($this, 'init_assets'));
+		add_action('init', array($this, 'init_sidebars'));
+	}
+
+	/**
+	 * Sets up and enqueues the scripts and styles for this theme
+	 * This function will be run on initialization
+	 * @return void
+	 * @since 1.0
+	 */
+	function init_assets() {
+
 		// Only enqueue files on the public part of the page 
 		if ( !is_admin() ) {
 			
-			wp_enqueue_style($this->plugin_name . '-style', get_bloginfo( 'stylesheet_directory') . "/style.css");
+			wp_enqueue_style($this->theme_name . '-style', get_bloginfo( 'stylesheet_directory') . "/style.css");
 
 			wp_enqueue_script('modernizr',  get_bloginfo("stylesheet_directory") . "/js/libs/modernizr-2.0.6.min.js");
-			wp_enqueue_script($this->plugin_name . '-plugins', get_bloginfo("stylesheet_directory") . "/js/plugins.js", array('jquery'), false, true);
-			wp_enqueue_script($this->plugin_name . '-script', get_bloginfo("stylesheet_directory") . "/js/script.js", array('jquery', $this->plugin_name . '-plugins'), false, true);
+			wp_enqueue_script($this->theme_name . '-plugins', get_bloginfo("stylesheet_directory") . "/js/plugins.js", array('jquery'), false, true);
+			wp_enqueue_script($this->theme_name . '-script', get_bloginfo("stylesheet_directory") . "/js/script.js", array('jquery', $this->theme_name . '-plugins'), false, true);
 		}
+	}
+
+	/**
+	 * Sets up the theme menu(s)
+	 * This function will be run on initialization
+	 * @return void
+	 * @since 1.0
+	 */
+	function init_menus() {
+		register_nav_menu('header', 'Huvudnavigationen');
+	}
+
+	/**
+	 * Sets up the theme sidebars
+	 * This function will be run on initialization
+	 * @return void
+	 * @since 1.0
+	 */
+	function init_sidebars() {
+
+		$args = array(
+			"name" 			=> "Högerspalten",
+			"id"			=> "sidebar",
+			"description" 	=> "Till höger om huvudinneh&aring;llet",
+			"before_widget" => "<section>",
+			"after_widget" 	=> "</section>",
+			"before_title" 	=> '<h1>',
+			"after_title" 	=> "</h1>"
+		);
+
+		register_sidebar($args);
 	}
 }
 
-add_action("init", create_function('', '$a = new Boilerplate();'));
+new Boilerplate();
 
 /**
  * Function to get the time in "relative format", i.e. xxx seconds/minutes/hours/... ago
@@ -55,5 +119,44 @@ function relative_time ($timestamp) {
 	
 	$text = "$difference $periods[$j] $ending";
 	
+	return $text;
+}
+
+/**
+ * Function to get the time in "relative format", i.e. xxx seconds/minutes/hours/... ago
+ * @param int $timestamp
+ * @return string
+ * @since 1.0
+ */
+function relative_time_sv ($timestamp) {
+
+    if (!is_numeric($timestamp)) {
+            return;
+    }
+
+	$difference = time() - $timestamp;
+	$period = array("sekund", "minut", "timme", "dag", "vecka", "månad", "år", "decennium");
+	$periods = array("sekunder", "minuter", "timmar", "dagar", "veckor", "månader", "år", "decennium");
+
+	$lengths = array("60","60","24","7","4.35","12","10");
+	$ending = $beginning = "";
+
+	if ($difference > 0) { // this was in the past
+		$ending = "sedan";
+		$beginning = "för";
+	} else { // this was in the future
+		$difference = -$difference;
+		$beginning = "om";
+	}
+
+	for($j = 0; $difference >= $lengths[$j]; $j++)
+		$difference /= $lengths[$j];
+
+	$difference = round($difference);
+
+	$unit = ($difference == 1) ? $period[$j] : $periods[$j];
+
+	$text = "$beginning $difference $unit $ending";
+
 	return $text;
 }
