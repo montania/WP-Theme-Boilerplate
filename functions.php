b83f96c7de74731b0669188ba05ac41652a16076
rickard <rickard@montania.se>
2014-08-12 11:50:05 +0200

    Lite PSR-2 och code-style

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Main file for the WordPress Theme
+ *
  * @author    Rickard Andersson <rickard@montania.se>
  * @copyright Montania System AB
  * @version   1.2
@@ -8,12 +9,14 @@
 
 /**
  * Main class for the WordPress Theme
+ *
  * @author Rickard Andersson <rickard@montania.se>
  */
-class Boilerplate {
-
+class Boilerplate
+{
     /**
      * Name of the directory where the theme files resides
+     *
      * @var string
      * @since 1.0
      */
@@ -21,48 +24,54 @@ class Boilerplate {
 
     /**
      * Will initialize the plugin function and add some hooks
+     *
      * @return Boilerplate
      * @since 1.0
      */
-    function __construct() {
-
-        add_action( 'init', array( $this, 'init_menus' ) );
-        add_action( 'init', array( $this, 'init_assets' ) );
-        add_action( 'init', array( $this, 'init_sidebars' ) );
+    public function __construct()
+    {
+        add_action('init', array($this, 'init_menus'));
+        add_action('init', array($this, 'init_assets'));
+        add_action('init', array($this, 'init_sidebars'));
     }
 
     /**
      * Sets up and enqueues the scripts and styles for this theme
      * This function will be run on initialization
+     *
      * @return void
      * @since 1.0
      */
-    function init_assets() {
-
-        $asset_revision=100;
+    public function init_assets()
+    {
+        $asset_revision = 100;
 
         // Only enqueue files on the public part of the page
-        if ( !is_admin() && !is_login_page() ) {
+        if (!is_admin() && !is_login_page()) {
 
-            wp_enqueue_style($this->theme_name . '-style',
-            	get_bloginfo('template_directory') . '/style.css', array(),
-            	$asset_revision);
+            wp_enqueue_style(
+                $this->theme_name . '-style',
+                get_bloginfo('template_directory') . '/style.css',
+                array(),
+                $asset_revision
+            );
 
-            wp_enqueue_script( 'modernizr', get_bloginfo( 'template_directory' ) . '/js/vendor/modernizr-2.6.3.min.js' );
-			// wp_enqueue_script(
-			// 	$this->theme_name . '-script',
-			// 	get_bloginfo('template_directory') . '/js/main.min.js',
-			// 	array('jquery'),
-			// 	$asset_revision,
-			// 	true
-			// );
+            wp_enqueue_script('modernizr', get_bloginfo('template_directory') . '/js/vendor/modernizr-2.6.3.min.js');
+            // wp_enqueue_script(
+            // 	$this->theme_name . '-script',
+            // 	get_bloginfo('template_directory') . '/js/main.min.js',
+            // 	array('jquery'),
+            // 	$asset_revision,
+            // 	true
+            // );
 
             if (is_child_theme()) {
                 wp_enqueue_style(
                     $this->theme_name . '-child-style',
                     get_bloginfo('stylesheet_directory') . '/style.css',
                     array($this->theme_name . '-style'),
-                    $asset_revision);
+                    $asset_revision
+                );
             }
         }
     }
@@ -70,21 +79,24 @@ class Boilerplate {
     /**
      * Sets up the theme menu(s)
      * This function will be run on initialization
+     *
      * @return void
      * @since 1.0
      */
-    function init_menus() {
-        register_nav_menu( 'header', 'Huvudnavigationen' );
+    public function init_menus()
+    {
+        register_nav_menu('header', 'Huvudnavigationen');
     }
 
     /**
      * Sets up the theme sidebars
      * This function will be run on initialization
+     *
      * @return void
      * @since 1.0
      */
-    function init_sidebars() {
-
+    public function init_sidebars()
+    {
         $args = array(
             'name'          => 'Högerspalten',
             'id'            => 'sidebar',
@@ -95,7 +107,7 @@ class Boilerplate {
             'after_title'   => '</h1>'
         );
 
-        register_sidebar( $args );
+        register_sidebar($args);
     }
 }
 
@@ -111,42 +123,45 @@ require 'class/opengraph.php';
  * @return string
  * @since 1.0
  */
-function relative_time( $timestamp ) {
+function relative_time($timestamp)
+{
 
-    if ( ! is_numeric( $timestamp ) ) {
+    if (!is_numeric($timestamp)) {
         return '';
     }
 
     $tz_string = get_option('timezone_string');
 
     if ($tz_string) {
-        $tz         = new DateTimeZone($tz_string);
-        $now        = new DateTime('now', $tz);
-        $posted     = DateTime::createFromFormat('U', $timestamp, $tz);
+        $tz     = new DateTimeZone($tz_string);
+        $now    = new DateTime('now', $tz);
+        $posted = DateTime::createFromFormat('U', $timestamp, $tz);
     } else {
-        $now        = new DateTime('now');
+        $now    = new DateTime('now');
         $posted = DateTime::createFromFormat('U', $timestamp);
     }
 
     $difference = strtotime($now->format('Y-m-d H:i:s')) - strtotime($posted->format('Y-m-d H:i:s'));
-    $periods    = array( 'sec', 'min', 'hour', 'day', 'week', 'month', 'year', 'decade' );
+    $periods    = array('sec', 'min', 'hour', 'day', 'week', 'month', 'year', 'decade');
 
-    $lengths = array( '60', '60', '24', '7', '4.35', '12', '10' );
+    $lengths = array('60', '60', '24', '7', '4.35', '12', '10');
 
-    if ( $difference > 0 ) { // this was in the past
+    if ($difference > 0) { // this was in the past
         $ending = 'ago';
     } else { // this was in the future
-        $difference = - $difference;
+        $difference = -$difference;
         $ending     = 'to go';
     }
 
-    for ( $j = 0; $difference >= $lengths[ $j ]; $j ++ )
-        $difference /= $lengths[ $j ];
+    for ($j = 0; $difference >= $lengths[$j]; $j++) {
+        $difference /= $lengths[$j];
+    }
 
-    $difference = round( $difference );
+    $difference = round($difference);
 
-    if ( $difference != 1 )
-        $periods[ $j ] .= 's';
+    if ($difference != 1) {
+        $periods[$j] .= 's';
+    }
 
     $text = "$difference $periods[$j] $ending";
 
@@ -161,44 +176,46 @@ function relative_time( $timestamp ) {
  * @return string
  * @since 1.0
  */
-function relative_time_sv( $timestamp ) {
+function relative_time_sv($timestamp)
+{
 
-    if ( ! is_numeric( $timestamp ) ) {
+    if (!is_numeric($timestamp)) {
         return '';
     }
 
     $tz_string = get_option('timezone_string');
 
     if ($tz_string) {
-        $tz         = new DateTimeZone($tz_string);
-        $now        = new DateTime('now', $tz);
-        $posted     = DateTime::createFromFormat('U', $timestamp, $tz);
+        $tz     = new DateTimeZone($tz_string);
+        $now    = new DateTime('now', $tz);
+        $posted = DateTime::createFromFormat('U', $timestamp, $tz);
     } else {
-        $now        = new DateTime('now');
+        $now    = new DateTime('now');
         $posted = DateTime::createFromFormat('U', $timestamp);
     }
 
     $difference = strtotime($now->format('Y-m-d H:i:s')) - strtotime($posted->format('Y-m-d H:i:s'));
-    $period     = array( 'sekund', 'minut', 'timme', 'dag', 'vecka', 'm&aring;nad', '&aring;r', 'decennium' );
-    $periods    = array( 'sekunder', 'minuter', 'timmar', 'dagar', 'veckor', 'm&aring;nader', '&aring;r', 'decennium' );
+    $period     = array('sekund', 'minut', 'timme', 'dag', 'vecka', 'm&aring;nad', '&aring;r', 'decennium');
+    $periods    = array('sekunder', 'minuter', 'timmar', 'dagar', 'veckor', 'm&aring;nader', '&aring;r', 'decennium');
 
-    $lengths = array( '60', '60', '24', '7', '4.35', '12', '10' );
+    $lengths = array('60', '60', '24', '7', '4.35', '12', '10');
     $ending  = $beginning = '';
 
-    if ( $difference > 0 ) { // this was in the past
+    if ($difference > 0) { // this was in the past
         $ending    = 'sedan';
         $beginning = 'för';
     } else { // this was in the future
-        $difference = - $difference;
+        $difference = -$difference;
         $beginning  = 'om';
     }
 
-    for ( $j = 0; $difference >= $lengths[ $j ]; $j ++ )
-        $difference /= $lengths[ $j ];
+    for ($j = 0; $difference >= $lengths[$j]; $j++) {
+        $difference /= $lengths[$j];
+    }
 
-    $difference = round( $difference );
+    $difference = round($difference);
 
-    $unit = ( $difference == 1 ) ? $period[ $j ] : $periods[ $j ];
+    $unit = ($difference == 1) ? $period[$j] : $periods[$j];
 
     $text = "$beginning $difference $unit $ending";
 
@@ -207,8 +224,10 @@ function relative_time_sv( $timestamp ) {
 
 /**
  * Returns true if the current page is the login page
+ *
  * @return bool
  */
-function is_login_page() {
+function is_login_page()
+{
     return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
 }
