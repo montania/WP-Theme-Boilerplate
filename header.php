2b316d76d90a6b0ac6d5bea77815e4ccb871f478
Johan Sölve <johan@montania.se>
2014-09-08 11:33:59 +0200

    Bort med överflödig lang="en"

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"<?php language_attributes(); ?>> <![endif]-->
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8" lang="en"<?php language_attributes(); ?>> <![endif]-->
+<html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>
-<html class="no-js lt-ie9" lang="en"<?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"<?php language_attributes(); ?>> <!--<![endif]-->
+<html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
