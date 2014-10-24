cca361ded107002f690510fb6d4ff177176d6d0a
Johan Sölve <johan@montania.se>
2014-10-24 09:40:11 +0200

    Stöd för svenska och engelska i entry-meta

--- entry-meta.php
+++ entry-meta.php
@@ -1,6 +1,10 @@
 <span class="entry-date">
-    Published
+    <?php _e('Published') ?>
     <time class="published updated" pubdate datetime="<?php echo date( "Y-m-d", get_the_date( 'U' ) ) ?>">
-        <?php echo relative_time( get_the_date( 'U' ) ); ?>
+        <?php 	if(explode('_', get_locale())[0] === 'sv') {
+			echo relative_time_sv( get_the_date( 'U' ) ); 
+		} else {
+			echo relative_time( get_the_date( 'U' ) ); 
+		} ?>
     </time>
 </span>
